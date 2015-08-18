e48f87beb43dde0678b09b238dce1bae8c34f51b
David Coster <David.Coster@ipp.mpg.de>
2015-08-18 14:31:46 +0200

    updates to the runs/Makefile so that all files are processed; fix for some scripts; add the missing label in solps.tex

--- scripts/compare_2_solps.py
+++ scripts/compare_2_solps.py
@@ -5,6 +5,19 @@ import matplotlib.pyplot as plt
 import argparse
 import patch
 
+def read_solps(shot, conn):
+  solps={'shot': shot}
+  conn.openTree('solps', solps['shot'])
+  solps['te']     = conn.get('\\top.snapshot:te').value
+  solps['ti']     = conn.get('\\top.snapshot:ti').value
+  solps['ne']     = conn.get('\\top.snapshot:ne').value
+  solps['na']     = conn.get('\\top.snapshot:na').value
+  solps['r']      = conn.get('\\top.snapshot.grid:r').value
+  solps['z']      = conn.get('\\top.snapshot.grid:z').value
+  solps['vessel'] = conn.get('\\top.snapshot.grid:vessel').value
+  conn.closeTree('solps', solps['shot'])
+  return solps
+
 parser=argparse.ArgumentParser(description="compare two SOLPS shots", formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument("Shot1", type=int, help="Reference shot number")
 parser.add_argument("Shot2", type=int, help="Comparison shot number")
@@ -12,36 +25,19 @@ args=parser.parse_args()
 
 conn = MDSplus.Connection('solps-mdsplus.aug.ipp.mpg.de:8001')
 
-conn.openTree('solps', args.Shot1)
-te_new=conn.get('\\top.snapshot:te').value
-ne_new=conn.get('\\top.snapshot:ne').value
-ti_new=conn.get('\\top.snapshot:ti').value
-na_new=conn.get('\\top.snapshot:na').value
-r_old=conn.get('\\top.snapshot.grid:r').value
-z_old=conn.get('\\top.snapshot.grid:z').value
-vessel_old=conn.get('\\top.snapshot.grid:vessel').value
-conn.closeTree('solps', args.Shot1)
+ref    = read_solps(args.Shot1, conn)
+comp   = read_solps(args.Shot2, conn)
 
-conn.openTree('solps', args.Shot2)
-te_old=conn.get('\\top.snapshot:te').value
-ne_old=conn.get('\\top.snapshot:ne').value
-ti_old=conn.get('\\top.snapshot:ti').value
-na_old=conn.get('\\top.snapshot:na').value
-r_new=conn.get('\\top.snapshot.grid:r').value
-z_new=conn.get('\\top.snapshot.grid:z').value
-vessel_new=conn.get('\\top.snapshot.grid:vessel').value
-conn.closeTree('solps', args.Shot2)
-
-if na_old.shape != na_new.shape:
-  print "Shape mismatch between old and new: ", na_old.shape, na_new.shape
+if ref['na'].shape != comp['na'].shape:
+  print "Shape mismatch between old and new: ", ref['na'].shape, comp['na'].shape
   exit()
 
-ns, ny, nx = na_new.shape
+ns, ny, nx = comp['na'].shape
 
-te_frac_err=2*(te_new-te_old)/(te_new+te_old)
-ne_frac_err=2*(ne_new-ne_old)/(ne_new+ne_old)
-ti_frac_err=2*(ti_new-ti_old)/(ti_new+ti_old)
-na_frac_err=2*(na_new-na_old)/(na_new+na_old)
+te_frac_err=2*(comp['te']-ref['te'])/(comp['te']+ref['te'])
+ne_frac_err=2*(comp['ne']-ref['ne'])/(comp['ne']+ref['ne'])
+ti_frac_err=2*(comp['ti']-ref['ti'])/(comp['ti']+ref['ti'])
+na_frac_err=2*(comp['na']-ref['na'])/(comp['na']+ref['na'])
 print na_frac_err.shape
 
 print "Maximum fractional error Te", numpy.abs(te_frac_err).max() 
@@ -49,6 +45,6 @@ print "Maximum fractional error ne", numpy.abs(ne_frac_err).max()
 print "Maximum fractional error Ti", numpy.abs(ti_frac_err).max() 
 print "Maximum fractional error na", numpy.abs(na_frac_err).max(axis=1).max(axis=1)
 
-patch.patch(te_frac_err, r_old, z_old, title='Te frac error %s/%s (Max %8.2e)' % (args.Shot1, args.Shot2, numpy.abs(te_frac_err).max()), flog=None, file='Te_frac_%s_%s.png' % (args.Shot1, args.Shot2))
-patch.patch(ne_frac_err, r_old, z_old, title='ne frac error %s/%s (Max %8.2e)' % (args.Shot1, args.Shot2, numpy.abs(ne_frac_err).max()), flog=None, file='ne_frac_%s_%s.png' % (args.Shot1, args.Shot2))
-patch.patch(ti_frac_err, r_old, z_old, title='Ti frac error %s/%s (Max %8.2e)' % (args.Shot1, args.Shot2, numpy.abs(ti_frac_err).max()), flog=None, file='Ti_frac_%s_%s.png' % (args.Shot1, args.Shot2))
+patch.patch(te_frac_err, ref['r'], ref['z'], title='Te frac error %s/%s (Max %8.2e)' % (args.Shot1, args.Shot2, numpy.abs(te_frac_err).max()), flog=None, file='Te_frac_%s_%s.png' % (args.Shot1, args.Shot2))
+patch.patch(ne_frac_err, ref['r'], ref['z'], title='ne frac error %s/%s (Max %8.2e)' % (args.Shot1, args.Shot2, numpy.abs(ne_frac_err).max()), flog=None, file='ne_frac_%s_%s.png' % (args.Shot1, args.Shot2))
+patch.patch(ti_frac_err, ref['r'], ref['z'], title='Ti frac error %s/%s (Max %8.2e)' % (args.Shot1, args.Shot2, numpy.abs(ti_frac_err).max()), flog=None, file='Ti_frac_%s_%s.png' % (args.Shot1, args.Shot2))
