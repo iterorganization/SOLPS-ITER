4778b63483f4e7826b782c75aba9e80fbd7dcdfc
Jeremy Lore <lorejd@ornl.gov>
2018-03-23 14:21:33 -0400

    Update to make python 3 and 2.7 compatible.

--- scripts/resall_D.py
+++ scripts/resall_D.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 #
 # run as:  resall_D.py [-l N]
 #
@@ -14,13 +14,13 @@ def main(args):
  
     file_in = "b2mn.exe.dir/b2ftrace" 
     if os.path.isfile(file_in):
-        print "Reading "+file_in
+        print("Reading "+file_in)
     else:
         file_in = "b2ftrace"
         if os.path.isfile(file_in):
-            print "Reading "+file_in            
+            print("Reading "+file_in)
         else:
-            print "Error: Could not find b2ftrace"
+            print("Error: Could not find b2ftrace")
             exit(0)
 
     with open(file_in) as f:
@@ -40,22 +40,25 @@ def main(args):
             mydatalist.extend(part_list)
         
     mydata = np.array(mydatalist)
-    mydata = mydata.reshape(counter,len(mydatalist)/counter)
+    mydata = mydata.reshape(counter,int(len(mydatalist)/counter))
 
     iend = np.size(mydata,0)
     if args.last > 0:
         iend = min(args.last,iend)
 
     plt.figure()
-    marker = itertools.cycle((',', '+', '.', 'o', '*')) 
-    plt.plot(mydata[0:iend,2],marker=marker.next(),label="conD0")
-    plt.plot(mydata[0:iend,3],marker=marker.next(),label="conD1")
-    plt.plot(mydata[0:iend,4],marker=marker.next(),label="momD0")
-    plt.plot(mydata[0:iend,5],marker=marker.next(),label="momD1")
-    plt.plot(mydata[0:iend,6],marker=marker.next(),label="totmom")
-    plt.plot(mydata[0:iend,7],marker=marker.next(),label="ee")
-    plt.plot(mydata[0:iend,8],marker=marker.next(),label="ei")
-    plt.plot(mydata[0:iend,9],marker=marker.next(),label="phi")
+    if iend > 100:
+        marker = itertools.cycle((None,None))
+    else:
+        marker = itertools.cycle((',', '+', '.', 'o', '*'))
+    plt.plot(mydata[0:iend,2],marker=next(marker),label="conD0")
+    plt.plot(mydata[0:iend,3],marker=next(marker),label="conD1")
+    plt.plot(mydata[0:iend,4],marker=next(marker),label="momD0")
+    plt.plot(mydata[0:iend,5],marker=next(marker),label="momD1")
+    plt.plot(mydata[0:iend,6],marker=next(marker),label="totmom")
+    plt.plot(mydata[0:iend,7],marker=next(marker),label="ee")
+    plt.plot(mydata[0:iend,8],marker=next(marker),label="ei")
+    plt.plot(mydata[0:iend,9],marker=next(marker),label="phi")
     plt.yscale('log')
     plt.xlabel("Iteration")
     plt.ylabel("norm of residuals")
