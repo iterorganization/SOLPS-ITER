0c5756d05e736b3ad1aee05601a34efc8a3ea4e6
Xavier Bonnin <Xavier.Bonnin@iter.org>
2018-12-05 10:03:56 +0100

    Applying the FORT variable to allow for HPUX default filenames

--- modules/Triang/src/src/triainq.f
+++ modules/Triang/src/src/triainq.f
@@ -21,6 +21,7 @@ c*  lnodes(3) : indices of their opposite nodes
 c*  lcell(2,:) : B2.5 grid indices
 c*  xr,yr : coordinates of the reference point
 
+      use eirmod_cinit, only: fort
       implicit none
       integer npoint,ntria,ltria
       real,allocatable :: px(:),py(:)
@@ -40,15 +41,9 @@ c*  xr,yr : coordinates of the reference point
 
 c=======================================================================
 c*** Open the data files
-#ifndef HPUX
-      open(21,file='fort.34',status='old',action='read')
-      open(22,file='fort.33',status='old',action='read')
-      open(23,file='fort.35',status='old',action='read')
-#else
-      open(21,file='ftn34',status='old',action='read')
-      open(22,file='ftn33',status='old',action='read')
-      open(23,file='ftn35',status='old',action='read')
-#endif
+      open(21,file=fort//'34',status='old',action='read')
+      open(22,file=fort//'33',status='old',action='read')
+      open(23,file=fort//'35',status='old',action='read')
 
 c*** Read the data from the input files
 
@@ -70,11 +65,8 @@ c*** Read the data from the input files
       read(21,*) ntria
       read(23,*) k
       if(k.ne.ntria) then !{
-#ifndef HPUX
-        stop 'ntria values in fort.34 and fort.35 files differ'
-#else
-        stop 'ntria values in ftn34 and ftn35 files differ'
-#endif
+        stop
+     .   'ntria values in '//fort//'34 and '//fort//'35 files differ'
       end if !}
       if(cell.gt.ntria) then !{
         write(0,*) 'cell > ntria :',cell,ntria
@@ -90,22 +82,14 @@ c*** read nodes
 
       read(22,*) px
       read(22,*) py
-#ifndef HPUX
-      write(0,*) '    reading fort.33 is finished'
-#else
-      write(0,*) '    reading ftn33 is finished'
-#endif
+      write(0,*) '    reading '//fort//'33 is finished'
 
 c*** read elements 
 
       do i=1,ntria   
         read(21,*) j,(tri(l,i),l=1,3)
       end do
-#ifndef HPUX
-      write(0,*) '    reading fort.34 is finished'
-#else
-      write(0,*) '    reading ftn34 is finished'
-#endif
+      write(0,*) '    reading '//fort//'34 is finished'
 c!###{
 c      do i=1,1   
 c      write(0,'(a,t12,4i8,1p/(2e17.8))') 'triangle',i,tri(1:3,i),
@@ -117,11 +101,7 @@ c*** read neighbours
       do i=1,ntria   
         read(23,*) j,(neigh(k,i),j,neigr(k,i),k=1,3),lcell(:,i)
       end do
-#ifndef HPUX
-      write(0,*) '    reading fort.35 is finished'
-#else
-      write(0,*) '    reading ftn35 is finished'
-#endif
+      write(0,*) '    reading '//fort//'35 is finished'
 c!###{
 c      do i=1,1   
 c      write(0,'(a,t12,4i8,1p/(2e17.8))') 'triangle',i,tri(1:3,i),
