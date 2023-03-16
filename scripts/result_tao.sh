1c6cea64bbde2c7ea21e1a95cb3a001bf59fd190
Stefano Carli <stefano.carli@kuleuven.be>
2023-03-16 09:18:20 +0100

    making scripts for extracting optimization results more robust

--- scripts/result_tao.sh
+++ scripts/result_tao.sh
@@ -1,20 +1,29 @@
 #! /bin/tcsh -f
-run_info; rm run.log
-cp b2mn.exe.dir/*.OUT .
-grep -i 'TAO,  Function value' run.info | awk '{print $5}' | awk -F"," '{print $1}' > objval.dat
-grep -i 'TAO GRADIENT NORM' run.info | awk '{print $4}' > grad.dat
+  if (-s run.log.gz) then
+    echo "Found run.log.gz, unzipping..."
+    gunzip run.log.gz
+  else if (-s run.log) then
+    echo "Found run.log"
+  else
+    echo "No run.log or run.log.gz file present, stopping"
+    exit
+  endif
+  run_info; rm run.log
+  cp b2mn.exe.dir/*.OUT .
+  grep -i 'TAO,  Function value' run.info | awk '{print $5}' | awk -F"," '{print $1}' > objval.dat
+  grep -i 'TAO GRADIENT NORM' run.info | awk '{print $4}' > grad.dat
 
-set npar_opt=`grep -i npar_opt run.info | awk '{print $3}'`
+  set npar_opt=`grep -i npar_opt run.info | awk '{print $3}'`
 
-rm -r tmp
-grep -i -B$npar_opt 'TAO,  Function value' run.info > tmp
-foreach jj ( `seq 1 $npar_opt` )
- grep -i "grad_F with x$jj=" tmp | awk '{print $5}' > parm$jj.dat
-end
-rm tmp
+  rm -r tmp
+  grep -i -B$npar_opt 'TAO,  Function value' run.info > tmp
+  foreach jj ( `seq 1 $npar_opt` )
+   grep -i "grad_F with x$jj=" tmp | awk '{print $5}' > parm$jj.dat
+  end
+  rm tmp
 
-foreach jj ( `seq 1 $npar_opt` )
- grep -i "grad_F with x$jj=" run.info | awk '{print $5}' > parm_hist$jj.dat
-end
+  foreach jj ( `seq 1 $npar_opt` )
+   grep -i "grad_F with x$jj=" run.info | awk '{print $5}' > parm_hist$jj.dat
+  end
 
-grep -i 'number of iterations' PETSC-TAO.OUT | awk -F'=' '{print $2}' | awk '{print $1}' > fgeval.dat
+  grep -i 'number of iterations' PETSC-TAO.OUT | awk -F'=' '{print $2}' | awk '{print $1}' > fgeval.dat
