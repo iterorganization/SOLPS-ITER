f3cb8c7125f78d835c7f28010a1583fa384d4591
Niels Horsten <niels.horsten@kuleuven.be>
2024-07-01 16:51:50 +0200

    Setting the regions for the unstructured file format

--- scripts/Balance/get_comuse.m
+++ scripts/Balance/get_comuse.m
@@ -13,12 +13,26 @@ finfo = ncinfo(filename);
 dimNames = {finfo.Dimensions.Name};
 dimMatch = strcmp(dimNames,'nCv');
 comuse.nCv = finfo.Dimensions(dimMatch).Length;
+dimMatch = strcmp(dimNames,'nCi');
+comuse.nCi = finfo.Dimensions(dimMatch).Length;
 dimMatch = strcmp(dimNames,'nFc');
 comuse.nFc = finfo.Dimensions(dimMatch).Length;
 dimMatch = strcmp(dimNames,'ns');
 comuse.ns = finfo.Dimensions(dimMatch).Length;
 dimMatch = strcmp(dimNames,'nstra');
 comuse.nstra = finfo.Dimensions(dimMatch).Length;
+dimMatch = strcmp(dimNames,'nXpt');
+comuse.nXpt = finfo.Dimensions(dimMatch).Length;
+dimMatch = strcmp(dimNames,'nimp');
+comuse.nimp = finfo.Dimensions(dimMatch).Length;
+dimMatch = strcmp(dimNames,'nomp');
+comuse.nomp = finfo.Dimensions(dimMatch).Length;
+% Regions, flux tubes, imp and omp
+comuse.cvReg = ncread(filename,'cvReg');
+comuse.cvFt = ncread(filename,'cvFt');
+comuse.ftCv = ncread(filename,'ftCv');
+comuse.imp = ncread(filename,'imp');
+comuse.omp = ncread(filename,'omp');
 % Species string:
 tmp = ncread(filename,'species');
 comuse.species = {};
@@ -43,4 +57,42 @@ comuse.cmap = ([0.0000    0.4470    0.7410;...
                 0.4660    0.6740    0.1880;...
                 0.3010    0.7450    0.9330;...
                 0.6350    0.0780    0.1840]);
+
+% Find flux tubes in SOL near separatrix
+if comuse.nXpt == 1 
+    for i = 1:comuse.nomp
+        if ((comuse.cvReg(comuse.omp(i)) == 2) || ...
+                (comuse.cvReg(comuse.omp(i)) == 6))
+            comuse.ftSep = comuse.cvFt(comuse.omp(i));
+            break;
+        end
+    end
+elseif comuse.nXpt == 2
+    warning('Only connected double null is supported')
+    comuse.ftSep = zeros(2,1);
+    for i = 1:comuse.nimp
+        if (comuse.cvReg(comuse.imp(i)) == 2)
+            comuse.ftSep(1) = comuse.cvFt(comuse.imp(i));
+            break;
+        end
+    end
+    for i = 1:comuse.nomp
+        if (comuse.cvReg(comuse.omp(i)) == 6)
+            comuse.ftSep(2) = comuse.cvFt(comuse.omp(i));
+            break;
+        end
+    end
+elseif comuse.nXpt == 0
+    for i = 1:comuse.nomp
+        if (comuse.cvReg(comuse.omp(i) == 2))
+            comuse.ftSep = comuse.cvFt(comuse.omp(i));
+            break;
+        end
+    end
+end
+
+
+
+
+
 
--- scripts/Balance/set_region.m
+++ scripts/Balance/set_region.m
@@ -1,63 +1,142 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % set_region sets the inrad and indpol logical grids for default region names. %
 %                                                                              %
-% David Moulton (david.moulton@ccfe.ac.uk) January 2017.                       %
+% David Moulton (david.moulton@ccfe.ac.uk) January 2017.      
+% Widegrid adaptation by Niels Horsten (niels.horsten@kuleuven.be) July 2024.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [indrad,indpol,reverse] = set_region(region_name,comuse)
 
-indrad = false(comuse.nx,comuse.ny);
-indpol = false(comuse.nx,comuse.ny);
+indrad = false(comuse.nCv,1);
+indpol = false(comuse.nCv,1);
 
-% Find the cuts to discern the geometry type:
-xcut = find(diff(comuse.leftix(:,1))<1);
-if length(xcut)==2
+% Determine the geometry type based on the nuumber of X-points
+if comuse.nXpt==1
     display('Assuming single null case');
     switch region_name(1:2)
         case {'li','ui'}
-            indrad(2:xcut(1)-1,2:end-1) = true;
-            indpol(2:xcut(1)-1,comuse.sep+2) = true;
+            for iCv = 1:comuse.nCi
+                if (comuse.cvReg(iCv) == 3)
+                    if (region_name(3)=='s')
+                        iFt = comuse.cvFt(iCv);
+                        if (any(comuse.cvReg(comuse.ftCv(iFt)) == 2) || ...
+                                any(comuse.cvReg(comuse.ftCv(iFt)) == 6))
+                            indrad(iCv) = true;
+                        end
+                    else
+                        indrad(iCv) = true;
+                    end
+                    if (comuse.cvFt(iCv) == comuse.ftSep)
+                        indpol(iCv) = true;
+                    end
+                end
+            end
             reverse = true;
         case {'uo','lo'}
-            indrad(xcut(2)+1:end-1,2:end-1) = true;
-            indpol(xcut(2)+1:end-1,comuse.sep+2) = true;
+            for iCv = 1:comuse.nCi
+                if ((comuse.cvReg(iCv) == 4) || (comuse.cvReg(iCv) == 8))
+                    if (region_name(3)=='s')
+                        iFt = comuse.cvFt(iCv);
+                        if (any(comuse.cvReg(comuse.ftCv(iFt)) == 2) || ...
+                                any(comuse.cvReg(comuse.ftCv(iFt)) == 6))
+                            indrad(iCv) = true;
+                        end
+                    else
+                        indrad(iCv) = true;
+                    end
+                    if (comuse.cvFt(iCv) == comuse.ftSep)
+                        indpol(iCv) = true;
+                    end
+                end
+            end
             reverse = false;
         otherwise
             error('Region name ''%s'' not supported.',region_name);
     end
-elseif length(xcut)==5
+elseif comuse.nXpt==2
     display('Assuming connected double null case');
     switch region_name(1:2)
         case 'li'
-            indrad(2:xcut(1)-1,2:end-1) = true;
-            indpol(2:xcut(1)-1,comuse.sep+2) = true;
+            for iCv = 1:comuse.nCi
+                if (comuse.cvReg(iCv) == 3)
+                    if (region_name(3)=='s')
+                        iFt = comuse.cvFt(iCv);
+                        if (any(comuse.cvReg(comuse.ftCv(iFt)) == 2))
+                            indrad(iCv) = true;
+                        end
+                    else
+                        indrad(iCv) = true;
+                    end
+                    if (comuse.cvFt(iCv) == comuse.ftSep(1))
+                        indpol(iCv) = true;
+                    end
+                end
+            end
             reverse = true;
         case 'ui'
-            indrad(xcut(2):xcut(3)-1,2:end-1) = true;
-            indpol(xcut(2):xcut(3)-1,comuse.sep+2) = true;
+            for iCv = 1:comuse.nCi
+                if (comuse.cvReg(iCv) == 4)
+                    if (region_name(3)=='s')
+                        iFt = comuse.cvFt(iCv);
+                        if (any(comuse.cvReg(comuse.ftCv(iFt)) == 2))
+                            indrad(iCv) = true;
+                        end
+                    else
+                        indrad(iCv) = true;
+                    end
+                    if (comuse.cvFt(iCv) == comuse.ftSep(1))
+                        indpol(iCv) = true;
+                    end
+                end
+            end
             reverse = false;
         case 'uo'
-            indrad(xcut(3)+2:xcut(4),2:end-1) = true;
-            indpol(xcut(3)+2:xcut(4),comuse.sep+2) = true;
+            for iCv = 1:comuse.nCi
+                if (comuse.cvReg(iCv) == 7)
+                    if (region_name(3)=='s')
+                        iFt = comuse.cvFt(iCv);
+                        if (any(comuse.cvReg(comuse.ftCv(iFt)) == 6))
+                            indrad(iCv) = true;
+                        end
+                    else
+                        indrad(iCv) = true;
+                    end
+                    if (comuse.cvFt(iCv) == comuse.ftSep(2))
+                        indpol(iCv) = true;
+                    end
+                end
+            end
             reverse = true;
         case 'lo'
-            indrad(xcut(5)+1:end-1,2:end-1) = true;
-            indpol(xcut(5)+1:end-1,comuse.sep+2) = true;
+            for iCv = 1:comuse.nCi
+                if (comuse.cvReg(iCv) == 8)
+                    if (region_name(3)=='s')
+                        iFt = comuse.cvFt(iCv);
+                        if (any(comuse.cvReg(comuse.ftCv(iFt)) == 6))
+                            indrad(iCv) = true;
+                        end
+                    else
+                        indrad(iCv) = true;
+                    end
+                    if (comuse.cvFt(iCv) == comuse.ftSep(2))
+                        indpol(iCv) = true;
+                    end
+                end
+            end
             reverse = false;
         otherwise
             error('Region name ''%s'' not supported.',region_name);
     end
-elseif isempty(xcut)
+elseif comuse.nXpt == 0
     display('Assuming continuous slab-like case');
     switch region_name(1:2)
         case {'li','ui','uo','lo'}
-            indrad(2:end-1,2:end-1) = true;
-            indpol(2:end-1,comuse.sep+1) = true;
+            indrad(1:nCi) = true;
+            if (region_name(3)=='s')
+                indrad(gmtry.cvReg == 1) = false;
+            end
+            indpol(comuse.cvFt == comuse.ftSep) = true;
             reverse = false;
         otherwise
             error('Region name ''%s'' not supported.',region_name);
     end
-end
-
-if region_name(3)=='s'
-    indrad(:,1:comuse.sep+1) = false;
 end
