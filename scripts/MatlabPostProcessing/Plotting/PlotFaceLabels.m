c55bb6584a6c50bce1b38343886cd763c735b879
Anthony Piras <anthony.piras@kuleuven.be>
2025-10-20 09:33:30 +0200

    Adding label legend to PlotFaceLabels. Speeding up plotgrid script.

--- scripts/MatlabPostProcessing/Plotting/PlotFaceLabels.m
+++ scripts/MatlabPostProcessing/Plotting/PlotFaceLabels.m
@@ -13,6 +13,8 @@ function PlotFaceLabels(gm,labellist,colorlist,type)
 
 hold on;
 N = length(labellist); % number of different labels provided
+legendHandles = gobjects(N,1);  % preallocation handles vector
+
 iflag = 0;
 nfaces = 0;
 for iFc = 1:gm.nFc
@@ -26,10 +28,41 @@ for iFc = 1:gm.nFc
         if gm.fcReg(iFc) == 0
 %             plot([x1 x2],[y1 y2],'k','LineWidth',0.5) 
         else
+            lbl = gm.fcReg(iFc);
+            if ischar(lbl) || isstring(lbl)
+                lblnum = str2double(lbl);
+            else
+                lblnum = double(lbl);
+            end
+            if isempty(lblnum) || isnan(lblnum) || lblnum == 0
+                continue;
+            end
             for i = 1:N
-                if gm.fcReg(iFc) == labellist(i)
-                    plot([x1 x2],[y1 y2],colorlist(i),'LineWidth',2)
-                    iflag = 1;
+                if lblnum == labellist(i)
+                    % estrai il colore correttamente (supporta cell array o matrice RGB)
+                    if iscell(colorlist)
+                        clr = colorlist{i};      % es. 'r' o '#FF0000'
+                    elseif isnumeric(colorlist) && size(colorlist,2) == 3
+                        clr = colorlist(i,:);    % RGB triplet
+                    else
+                        % fallback (es. char array di short codes)
+                        clr = colorlist(i);
+                    end
+                    % plot usando 'Color' per essere coerenti con RGB o char
+                    if ischar(clr) || isstring(clr)
+                        h = plot([x1 x2],[y1 y2], 'Color', char(clr), 'LineWidth', 2);
+                    else
+                        % assume clr è un vettore RGB 1x3
+                        h = plot([x1 x2],[y1 y2], 'Color', clr, 'LineWidth', 2);
+                    end
+                    nfaces = nfaces + 1;
+                    if (iflag==0) 
+                        iflag = 1;
+                    end
+                    % salva SOLO il primo handle valido per questa label
+                    if ~isgraphics(legendHandles(i))
+                        legendHandles(i) = h;
+                    end
                 end
             end
         end
@@ -37,19 +70,53 @@ for iFc = 1:gm.nFc
         if gm.fcLbl(iFc) == 0
             %plot([x1 x2],[y1 y2],'k','LineWidth',0.5) 
         else
+            lbl = gm.fcLbl(iFc);
+            if ischar(lbl) || isstring(lbl)
+                lblnum = str2double(lbl);
+            else
+                lblnum = double(lbl);
+            end
+            if isempty(lblnum) || isnan(lblnum) || lblnum == 0
+                continue;
+            end
             for i = 1:N
-                if gm.fcLbl(iFc) == labellist(i)
-                    plot([x1 x2],[y1 y2],colorlist(i),'LineWidth',2)
-                    iflag = 1;
+                if lblnum == labellist(i)
+                    % estrai il colore correttamente (supporta cell array o matrice RGB)
+                    if iscell(colorlist)
+                        clr = colorlist{i};      % es. 'r' o '#FF0000'
+                    elseif isnumeric(colorlist) && size(colorlist,2) == 3
+                        clr = colorlist(i,:);    % RGB triplet
+                    else
+                        % fallback (es. char array di short codes)
+                        clr = colorlist(i);
+                    end
+                    % plot usando 'Color' per essere coerenti con RGB o char
+                    if ischar(clr) || isstring(clr)
+                        h = plot([x1 x2],[y1 y2], 'Color', char(clr), 'LineWidth', 2);
+                    else
+                        % assume clr è un vettore RGB 1x3
+                        h = plot([x1 x2],[y1 y2], 'Color', clr, 'LineWidth', 2);
+                    end
                     nfaces = nfaces + 1;
-
+                    if (iflag==0) 
+                        iflag = 1;
+                    end
+                    % salva SOLO il primo handle valido per questa label
+                    if ~isgraphics(legendHandles(i))
+                        legendHandles(i) = h;
+                    end
                 end
             end
         end
     else
         error('type not recognized')
     end
+end
 
+validIdx = isgraphics(legendHandles);
+if any(validIdx)
+    legendLabels = arrayfun(@(x) num2str(x), labellist(validIdx), 'UniformOutput', false);
+    legend(legendHandles(validIdx), legendLabels, 'Location', 'best');
 end
 
 if iflag==0
