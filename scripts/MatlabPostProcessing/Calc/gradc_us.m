0501e11d590956d0d44430cbcb246e2dd140b321
Anthony Piras <anthony.piras@kuleuven.be>
2026-01-12 14:46:02 +0100

    Updates for MATLAB post-processing routines: - bugfix for quiverplot_bfc - added possibility to change the arrow scaling in quiverplot_bf and quiverplot_bfc - corrected indentation in intcell_us - bugfix for gradc_us - updated vxVol_style in calc_vxVol routine to be consistent with WG code - added auxiliary routine compute_cvFpsi needed for calc_vxVol

--- scripts/MatlabPostProcessing/Calc/gradc_us.m
+++ scripts/MatlabPostProcessing/Calc/gradc_us.m
@@ -1,10 +1,17 @@
-function [gfun] = gradc_us(gmtry,mode,fun,volstyle)
+function [gfun] = gradc_us(gmtry,mode,fun,vxVol_style,funv)
 % [gfun] = gradc_us(gmtry,mode,fun,volstyle)
 %
 % Computes the gradient of cell-centered quantity on cell-centers; the
 % poloidal component gfun(:,1) and the radial component gfun(:,2) are
 % returned.
 %
+
+if ~exist('funv','var') || isempty(funv)
+    if mode ~= 0
+        error(['funv not passed as input or empty && mode = 0 not allowed.' ...
+            ' Please provide funv or set mode = 0.'])
+    end
+end
 
 nCv = gmtry.nCv;
 nFc = gmtry.nFc;
@@ -18,11 +25,12 @@ end
 
 %   ..interpolate fun to cell vertices, using volume weighted averaging
 if (mode==0)
-    vxVol = calc_vxVol(nVx,gmtry,volstyle);
+    vxVol = calc_vxVol(nVx,gmtry,vxVol_style);
     [funv] = intvertex_us(nVx,gmtry,vxVol,fun);
 end
 
 %   ..compute poloidal and radial gradients
 [gfun(:,1)]=gradc_p_us(nCv, nFc, nVx, 1, gmtry, fun, funv);
 [gfun(:,2)]=gradc_r_us(nCv, nFc, nVx, 1, gmtry, fun, funv);
+
 end
